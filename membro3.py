0c5d912222ee64a2d02d1d35cfa7d4437d02c942
gabsbsbsbs <gabrielsalmoria1@gmail.com>
2023-08-10 15:30:59 -0300

    atualizando membro 3

--- membro3.py
+++ membro3.py
@@ -1,2 +1,4 @@
 def hello():
-	return "Olá, eu sou o membro3" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, sou o Gabriel, tenho 18 anos, entrei na UFSC em 23.1 no curso de Ciências da Computação. Tenho uma experiência bem básica com programação, mas tenho um grande interesse em aprender dentro da área. Gosto de tecnologia, aprender sobre psicologia/filosofia e gosto de jogar videogame e sair com os amigos!" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+
+gabsbsbsbs
