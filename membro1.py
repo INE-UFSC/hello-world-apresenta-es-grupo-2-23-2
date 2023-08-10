e3540e3830b94e6aa6b300eb46beeac47fbfbc28
RafaelOliveira93 <141952841+RafaelOliveira93@users.noreply.github.com>
2023-08-10 15:02:19 -0300

    Update membro1.py

--- membro1.py
+++ membro1.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou a Laura, tenho 22 anos, entrei na UFSC em 19.1 para cursar engenharia elétrica, e transferi para computação esse ano. Também curso Design Gráfico na Univali. Gosto de fotografia analógica, tenho pouca experiência com programação. 
