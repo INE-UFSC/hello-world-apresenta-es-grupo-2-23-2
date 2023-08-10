2cbbd6cefc05552b53ffb2b9d10847fd0158ac49
gustavo nunes viana <nunesvianagustavo@gmail.com>
2023-08-10 11:37:28 -0700

    adicionando membro 4

---
+++ membro4.py
@@ -0,0 +1,2 @@
+def hello():
+	return "eu sou o gustavo tenho 18 anos, gosto de jogar um xadrezinho possuo pouca experiencia mas muita vontade de descobrir areas interessantes, e essa materia sera fundamental para isso "
