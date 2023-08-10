e7226a11b2d6ed3f0c4ac8bbfdf7cf02e547e3f6
RafaelOliveira93 <rafael.oliveira.r.s@grad.ufsc.br>
2023-08-10 15:22:35 -0300

    Atualizando a apresentação do membro 2

--- membro2.py
+++ membro2.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Rafael, tenho 20 anos e moro no Norte da ilha. Gosto de jogar jogos do estilo moba e FPS e já fiz um curso técnico em computação." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
